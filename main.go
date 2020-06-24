3c1b109c6eb2df961bd47e544488c41208b11e1d
Ang Chin Han <angch@tuxuri.com>
2020-06-24 10:55:04 +0800

    feat: Remember old posts, and don't post dupes

--- main.go
+++ main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"bytes"
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
@@ -24,17 +26,12 @@ func doYMD(y, m, d int) *ApodPost {
 	}
 
 	url := fmt.Sprintf("https://apod.nasa.gov/apod/ap%02d%02d%02d.html", y, m, d)
-
-	log.Println(url)
 	resp, err := http.Get(url)
 	if err != nil {
 		log.Fatal(err)
 	}
-	if resp.Status != "200" {
-		log.Fatal("Not ready yet")
-	}
-	if true {
-		log.Fatal("foom")
+	if resp.StatusCode != 200 {
+		log.Fatal("Not ready yet ", resp.Status)
 	}
 	doc, err := goquery.NewDocumentFromReader(resp.Body)
 	if err != nil {
@@ -79,14 +76,49 @@ func main() {
 	if slackUrl == "" {
 		log.Fatal("Env SLACKWEBHOOK required, e.g. https://hooks.slack.com/services/z/y/x")
 	}
-	today := time.Now()
+	posts := make(map[string]ApodPost)
+	f, err := os.Open("posts.js")
+	if err == nil {
+		b, err := ioutil.ReadAll(f)
+		if err != nil {
+			log.Fatal(err)
+		}
+		err = json.Unmarshal(b, &posts)
+		if err != nil {
+			log.Fatal(err)
+		}
+		f.Close()
+	}
+
+	today := time.Now() // Yes, I know. timezone.
 	d := today.Day()
 	m := int(today.Month())
 	y := today.Year()
+	key := fmt.Sprintf("%04d%02d%02d", y, m, d)
+	_, exists := posts[key]
+	if exists {
+		log.Println("Done for today")
+		return
+	}
 
 	p := doYMD(y, m, d)
-	log.Printf("%+v\n", p)
 	if p != nil {
-		postSlack(p)
+		posts[key] = *p
+		f, err := os.OpenFile("posts.js", os.O_RDWR|os.O_CREATE, 0755)
+		if err != nil {
+			log.Fatal(err)
+		}
+		b, _ := json.Marshal(posts)
+		_, err = f.Write(b)
+		if err != nil {
+			log.Fatal(err)
+		}
+		f.Close()
+
+		log.Printf("%+v\n", p)
+
+		if true {
+			postSlack(p)
+		}
 	}
 }
